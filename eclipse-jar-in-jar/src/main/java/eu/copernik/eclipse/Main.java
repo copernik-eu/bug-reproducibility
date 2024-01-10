6835d6251f044f29245e61fcfcfd4471d5176ff7
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-01-10 09:56:32 +0100

    Improve output

--- eclipse-jar-in-jar/src/main/java/eu/copernik/eclipse/Main.java
+++ eclipse-jar-in-jar/src/main/java/eu/copernik/eclipse/Main.java
@@ -2,12 +2,9 @@ package eu.copernik.eclipse;
 
 import java.io.IOException;
 import java.net.JarURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.jar.JarFile;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Main {
 
@@ -23,15 +20,18 @@ public class Main {
 
     private static void printURL(final URL url) throws IOException {
         System.out.println("Testing URL " + url);
+        System.out.println();
         final URLConnection urlConnection = url.openConnection();
         System.out.println("URLConnection type: " + urlConnection.getClass());
         if (urlConnection instanceof JarURLConnection) {
             final JarURLConnection jarURLConnection = (JarURLConnection) urlConnection;
+            System.out.println("JAR file URL: " + jarURLConnection.getJarFileURL());
             final JarFile jarFile = jarURLConnection.getJarFile();
             System.out.println("JAR file type: " + jarFile.getClass());
             System.out.println("JAR file name: "  + jarFile.getName());
             System.out.println("Multi-Release: " + jarFile.isMultiRelease());
             System.out.println("Multi-Release version: " + jarFile.getVersion());
         }
+        System.out.println();
     }
 }
